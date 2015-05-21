c953961ad4cca03eb765b6a0cab61090e118514d
mak <mak@cert.pl>
2015-05-21 18:29:04 +0200

    server: rember few last changes and dont send duplicats

--- srv/server.py
+++ srv/server.py
@@ -1,4 +1,5 @@
 import zmq
+import threading,Queue
 
 ctx = zmq.Context()
 out_sock = ctx.socket(zmq.PUB)
@@ -10,6 +11,22 @@ in_sock.bind('tcp://*:1337')
 
 poller = zmq.Poller()
 poller.register(in_sock,zmq.POLLIN)
+Q = Queue.Queue()
+def sender():
+    last_hashes=[]
+    while True:
+        msg = Q.get()
+        print msg
+        
+        if msg['hash'] in last_hashes: continue
+        last_hashes.append(msg['hash'])
+        del msg['hash']
+        out_sock.send_json(msg)
+
+        if len(last_hashes) > 500:
+            last_hashes = last_hashes[-500:]
+ts = threading.Thread(target=sender)
+ts.start()
 
 while True:
     try:
@@ -20,6 +37,4 @@ while True:
     
     if in_sock in ev:
         msg = in_sock.recv_json()
-        print msg
-        print in_sock.getsockopt(zmq.IDENTITY)
-        out_sock.send_json(msg)
+        Q.put(msg)
