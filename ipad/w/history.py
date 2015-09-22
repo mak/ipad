95e43094960caa695dc4a8796b8d552698280f85
mak <mak@cert.pl>
2015-09-22 20:36:16 +0200

    history: mockup for ctx menu acctions

--- ipad/w/history.py
+++ ipad/w/history.py
@@ -14,6 +14,13 @@ class WHistory(QtGui.QMainWindow):
         
         self._prepare()
 
+        self.hist_list.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.connect(self.hist_list,QtCore.SIGNAL('customContextMenuRequested(QPoint)'), self.ctxMenu)
+        self.acc_apply.triggered.connect(self.do_acc_apply)
+        self.popMenu.addAction(self.acc_apply)
+        self.acc_remov.triggered.connect(self.do_acc_remov)
+        self.popMenu.addAction(self.acc_remov)
+        
         widg = QtGui.QWidget()
         wlay = QtGui.QVBoxLayout()
         wlay.addWidget(self.hist_list)
@@ -25,17 +32,20 @@ class WHistory(QtGui.QMainWindow):
     def setDB(self,db):
         self.db = db
 
+    def setUser(self,u):
+        self.user = u
+        
     def _clear(self):
         self.hist_elem.clear()
         self.hist_list.clear()
         self.hist_list.setRowCount(0)
         self.hist_elem.setRowCount(0)
-        self.hist_list_label = ['timestamp','action']
+        self.hist_list_label = ['timestamp','action','origin']
         self.hist_list.setColumnCount(len(self.hist_list_label))
         self.hist_list.setHorizontalHeaderLabels(self.hist_list_label)
         
     def _populate(self,array):
-        self.hist_list_label = ['timestamp','action']
+        self.hist_list_label = ['timestamp','action','origin']
         self._clear()
     
         self.hist_list.setRowCount(len(array))
@@ -46,6 +56,8 @@ class WHistory(QtGui.QMainWindow):
                     itm = QtGui.QTableWidgetItem(str(datetime.datetime.fromtimestamp(el['timestamp'])))
                 elif cl == 1:
                     itm = QtGui.QTableWidgetItem(el['action'])
+                elif cl ==2:
+                    itm = QtGui.QTableWidgetItem('local' if el['user'] == self.user else 'remote')
                 self.hist_list.setItem(row,cl,itm)
             self.hist_list.resizeRowToContents(row)
         self.hist_list.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)    
@@ -60,6 +72,8 @@ class WHistory(QtGui.QMainWindow):
                 itm = QtGui.QTableWidgetItem(str(datetime.datetime.fromtimestamp(el['timestamp'])))
             elif cl == 1:
                 itm = QtGui.QTableWidgetItem(el['action'])
+            elif cl ==2:
+                itm = QtGui.QTableWidgetItem('local' if el['user'] == self.user else 'remote')
             self.hist_list.setItem(nrow,cl,itm)
         self.hist_list.resizeRowToContents(nrow)
         self.hist_list.resizeColumnsToContents()
@@ -85,13 +99,23 @@ class WHistory(QtGui.QMainWindow):
         self.hist_elem.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)    
         self.hist_elem.resizeColumnsToContents()
 
-
             
     def _prepare(self):
         self.hist_list = QtGui.QTableWidget()
         self.hist_list.clicked.connect(self._onHistClicked)
-
         self.hist_elem = QtGui.QTableWidget()
+        self.popMenu = QtGui.QMenu(self)
+        self.acc_apply = QtGui.QAction('apply', self)
+        self.acc_remov =QtGui.QAction('remove', self)
+
         
+    def ctxMenu(self,point):
+        print 'hophop'
+        self.popMenu.exec_(self.hist_list.mapToGlobal(point))
 
+    def do_acc_apply(self):
+        print 'apply'
+    def do_acc_remov(self):
+        print 'remove'
 
+            
