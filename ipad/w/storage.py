7c1851b9150b30cc9b947220c83984e7c446a782
mak <mak@cert.pl>
2015-09-24 18:28:16 +0200

    widget to load binaries/idbs from remote

--- ipad/w/storage.py
+++ ipad/w/storage.py
@@ -3,38 +3,67 @@ from PySide import QtGui, QtCore
 from PySide.QtGui import QIcon
 import datetime,json
 
+class MyItem(QtGui.QStandardItem):
 
+    def __init__(self,*a,**k):
+        super(MyItem,self).__init__(*a,**k)
+        self.__data= None
+
+    @property
+    def xdata(self):
+        return self.__data
+    @xdata.setter
+    def xdata(self,v):
+        self.__data = v
+        
 class WStorage(QtGui.QMainWindow):
 
     def __init__(self,parent):
         QtGui.QMainWindow.__init__(self)
         self.parent = parent
-        self.name = 'History'
+        self.name = 'Storage'
         self.icon = QIcon('')
         self.rcount = 0
 
         self.treeView = QtGui.QTreeView()
         self.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.treeView.customContextMenuRequested.connect(self.openMenu)
-        
+        self.treeView.doubleClicked.connect(self.dclick)
         self.model = QtGui.QStandardItemModel()
-        self.addItems(self.model, data)
         self.treeView.setModel(self.model)
         
         self.model.setHorizontalHeaderLabels([self.tr("Object")])
         
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.treeView)
-        self.setLayout(layout)
-    
-    def addItems(self, parent, elements):
-    
-        for text, children in elements:
-            item = QtGui.StandardItem(text)
-            parent.appendRow(item)
-            if children:
-                self.addItems(item, children)
-    
+        widg=QtGui.QWidget()
+        widg.setLayout(layout)
+        self.setCentralWidget(widg)
+
+        
+    def _add_idb(self, n,parent, elements):
+        item = QtGui.QStandardItem(n)
+        parent.appendRow(item)
+        for el in elements:
+            tm = datetime.datetime.fromtimestamp(int(el['timestamp'])).strftime('%T %D')
+            obj = MyItem('%s - %s' % (el['tag'],tm))
+            obj.xdata = el
+            item.appendRow(obj)
+            # if children:
+            #     self.addItems(item, children)
+
+    def _populate(self,data):
+        for n,v in data.iteritems():
+            self._add_idb(n,self.model,v)
+
+    def setCtrl(self,c):
+        self.ctl = c
+
+    def dclick(self,idx):
+        m = idx.model()
+        data = m.item(idx.column()).child(idx.row()).xdata
+        self._load_idb(data)
+        
     def openMenu(self, position):
     
         indexes = self.treeView.selectedIndexes()
@@ -45,14 +74,22 @@ class WStorage(QtGui.QMainWindow):
             while index.parent().isValid():
                 index = index.parent()
                 level += 1
-        
+                
         menu = QtGui.QMenu()
-        if level == 0:
-            menu.addAction(self.tr("Edit person"))
-        elif level == 1:
-            menu.addAction(self.tr("Edit object/container"))
-        elif level == 2:
-            menu.addAction(self.tr("Edit object"))
-        
+        if level == 1:
+            idx = indexes[0]
+            m = idx.model()
+            data = m.item(idx.column()).child(idx.row()).xdata
+            acc =  QtGui.QAction("Load",self)
+            f_action = lambda : self._load_idb(data)
+            menu.addAction(acc)
+            acc.triggered.connect(f_action)
+            
         menu.exec_(self.treeView.viewport().mapToGlobal(position))
 
+    def _load_idb(self,data):
+        # print data
+        # print 'loaded'
+        self.parent.ctrl.load_idb(data['uid'],data['ssid'])
+
+        
